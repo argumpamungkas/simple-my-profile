4a7a0c5d227e19fabb11d0780e562c88175725de
argumpamungkas <argumpamungkaass@gmail.com>
2023-05-05 10:22:59 +0700

    update parallax

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-// event pada di link
-
+// event pada di link a navbar
 $(".page-scroll").on("click", function (e) {
   var hrefTujuan = $(this).attr("href");
   var elementHrefTujuan = $(hrefTujuan);
@@ -13,4 +12,45 @@ $(".page-scroll").on("click", function (e) {
   );
 
   e.preventDefault();
+});
+
+// Parallax header
+
+$(window).on("load", function () {
+  $(".jumbotron").addClass("show");
+});
+
+// Parallax about & gallery
+$(window).scroll(function () {
+  let wScroll = $(this).scrollTop();
+
+  // Header
+
+  $(".jumbotron img").css({
+    transform: "translate(0px, " + wScroll / 8 + "%)",
+  });
+
+  $(".jumbotron h1").css({
+    transform: "translate(0px, " + wScroll / 4.2 + "%)",
+  });
+
+  $(".jumbotron p").css({
+    transform: "translate(0px, " + wScroll / 2.5 + "%)",
+  });
+
+  console.log(wScroll);
+
+  // about
+  if (wScroll > $(".about").offset().top - 400) {
+    $(".about p").addClass("show");
+  }
+
+  // Gallery
+  if (wScroll > $(".gallery").offset().top - 400) {
+    $(".gallery .thumbnail").each(function (i) {
+      setTimeout(function () {
+        $(".gallery .thumbnail").eq(i).addClass("show");
+      }, 500 * (i + 1));
+    });
+  }
 });
